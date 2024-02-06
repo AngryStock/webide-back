63d0a2d7d34827547b7a37c175bd7e46be3ca3e0
Minjeong63 <kmj06030@naver.com>
2024-02-06 16:24:55 +0900

    Feat: 비밀번호 유효성 규칙 추가

--- src/main/java/king/ide/controller/request/SignupRequest.java
+++ src/main/java/king/ide/controller/request/SignupRequest.java
@@ -14,6 +14,7 @@ public class SignupRequest {
     private String loginId;
 
     @NotEmpty(message = "비밀번호를 입력해 주세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$", message = "비밀번호는 8자 이상, 16자 이하의 영문, 숫자 및 특수문자를 조합하여 사용해야 합니다.")
     private String password;
 
     @NotEmpty(message = "휴대폰번호를 입력해 주세요.")
