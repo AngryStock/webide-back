96f252959518f69ffc45ddc28d5f29c07f5a700d
Minjeong63 <kmj06030@naver.com>
2024-02-06 16:12:40 +0900

    Feat: 유효성 검사 추가

--- src/main/java/king/ide/controller/request/SignupRequest.java
+++ src/main/java/king/ide/controller/request/SignupRequest.java
@@ -1,19 +1,21 @@
 package king.ide.controller.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class SignupRequest {
-    @NotEmpty
+    @NotEmpty(message = "이름을 입력해 주세요.")
+    @Pattern(regexp = "^[a-zA-Z가-힣\\\\s]+$", message = "이름은 문자만 입력할 수 있습니다.")
     private String name;
 
-    @NotEmpty
+    @NotEmpty(message = "아이디를 입력해 주세요.")
     private String loginId;
 
-    @NotEmpty
+    @NotEmpty(message = "비밀번호를 입력해 주세요.")
     private String password;
 
-    @NotEmpty
+    @NotEmpty(message = "휴대폰번호를 입력해 주세요.")
     private String mobileNumber;
 }
