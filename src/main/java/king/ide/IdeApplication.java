04776566f2065a4120fd651fe90b03aeb89f64ab
goodnight <qjadjehd05@gmail.com>
2024-02-19 11:45:08 +0900

    Feat: 엔티티 생성/수정 날짜 자동생성용 추상 클래스 추가, 채팅 조회 핸들러매핑 추가

--- src/main/java/king/ide/IdeApplication.java
+++ src/main/java/king/ide/IdeApplication.java
@@ -3,7 +3,9 @@ package king.ide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class IdeApplication {
 
