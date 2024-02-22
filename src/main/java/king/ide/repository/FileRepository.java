31c4090631b80164846c8b7534dca20407a82fe9
hearinhan <gksk11104@gmail.com>
2024-02-22 23:48:36 +0900

    Feat: 코드 에디터 트리 구조 관련 파일 업로드 및 repository 수정

--- src/main/java/king/ide/repository/FileRepository.java
+++ src/main/java/king/ide/repository/FileRepository.java
@@ -4,7 +4,12 @@ import king.ide.domain.Files;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FileRepository extends JpaRepository<Files, Long> {
     // Spring Data JPA 사용
+
+    // 추가
+    List<Files> findByParentFolderId(Long parentFolderId);
 }
--- src/main/java/king/ide/repository/PackageRepository.java
+++ src/main/java/king/ide/repository/PackageRepository.java
@@ -4,7 +4,12 @@ import king.ide.domain.Packages;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PackageRepository extends JpaRepository<Packages, Long> {
     // Spring Data JPA 사용
+
+    // 추가
+    List<Packages> findByParentPackageId(Long parentPackageId);
 }
